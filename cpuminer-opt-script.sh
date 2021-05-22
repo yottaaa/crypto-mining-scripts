af364ca7ed53f808a9fe733e34d19d7a8fc013ce
yottaaa <yotterick7@gmail.com>
2021-05-22 14:32:23 +0800

    Change clone repository

--- cpuminer-opt-script.sh
+++ cpuminer-opt-script.sh
@@ -12,15 +12,15 @@ if [[ $EUID = 0 ]]; then
 	
 	apt-get --assume-yes install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++
 
-	git clone https://github.com/tpruvot/cpuminer-multi
+	git clone https://github.com/JayDDee/cpuminer-opt.git
 
-	cd cpuminer-multi
+	cd cpuminer-opt
 
 	./build.sh
 
-	cd cpuminer
+	cd ../cpuminer
 
-	cd cpuminer-multi
+	cd ../cpuminer-opt
 
 else
 	echo "Please run with sudo"
